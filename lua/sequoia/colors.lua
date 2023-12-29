9341f8c0f10479c5bb2871897e7169544046c06a
keu-5 <keu02.78@gmail.com>
2023-12-29 14:20:55 +0900

    update

--- lua/sequoia/colors.lua
+++ lua/sequoia/colors.lua
@@ -13,7 +13,7 @@ function M.setup()
         interface = "#111216",
         text = "#b4b4c2",
         slight = "#575861",
-        soft = "#ffbb88",
+        soft = "#ffffbb",
 
         focus_high = "#1F1F24",
 
